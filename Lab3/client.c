fdf7291157ff0a06827e033cae73106f15c9e610
Faheem Hassan Zunjani <faheemzunjani@gmail.com>
2016-09-09 16:23:37 +0530

    Add client.c

---
+++ Lab3/client.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	int a,b;
+	char ch;
+	printf("Enter values and sign\n");
+	scanf("%d%d%c%c",&a,&b,&ch,&ch);
+	int pid=fork();
+	int status;
+	if(pid>0)
+	{
+		wait(&status);
+		printf("answer = %d\n",status);
+
+	}
+	else
+	{
+		execl("/home/debadutt
+	
+
+
+	}
+	return 0;
+}
