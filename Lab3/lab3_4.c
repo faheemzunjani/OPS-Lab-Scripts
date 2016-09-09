903b634d316955b458aa7a5a8c73730855e787e1
Faheem Hassan Zunjani <faheemzunjani@gmail.com>
2016-09-09 16:23:01 +0530

    Add lab3_4.c

---
+++ Lab3/lab3_4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	int pid=fork();	
+	int a=5;
+	if(pid > 0)
+		printf("%p parent\n",&a);
+	else
+		printf("%p child\n",&a);
+
+
+	while(1);
+
+
+
+
+
+
+
+
+
+	return 0;
+}
