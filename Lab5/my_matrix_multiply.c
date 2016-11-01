9a799361f92e50b2ccdba67db46bf690a58d7f93
Faheem Zunjani <faheemzunjani@gmail.com>
2016-11-01 12:40:12 +0530

    Finished my_matrix_multiply.c bug fixes

--- Lab5/my_matrix_multiply.c
+++ Lab5/my_matrix_multiply.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-struct arg {
-	int r_A;
-	int c_A;
-	int r_B;
-	int c_B;
-	int t_c;
-	int t_n;
-};
-
+int r_A;
+int c_A;
+int r_B;
+int c_B;
+int t_c;
+int t_n;
 double A[100][100];
 double B[100][100];
 double C[100][100];
+int t_jobs[100];
 
 void * threaded_multiply(void *);
 
@@ -21,34 +19,23 @@ int main(int argc, char ** argv)
 {
 	FILE * fptr1;
 	FILE * fptr2;
-	int thread_c;
 	pthread_t threads[100];
 	int i; 
 	int j;
-	int r_A;
-	int c_A;
-	int r_B;
-	int c_B;
 	char readString[100];
-	struct arg arg_t;
-	struct arg * arg_ptr = NULL;
 	int t_status;
+	int temp;
+	int temp2;
 
 	fptr1 = fopen(argv[2], "r");
 	fptr2 = fopen(argv[4], "r");
-	thread_c = atoi(argv[6]);
+	t_c = atoi(argv[6]);
 	i = 0; 
 	j = 0;
-	arg_t.r_A = r_A;
-	arg_t.r_B = r_B;
-	arg_t.c_A = c_A;
-        arg_t.c_B = c_B;
-	arg_t.t_c = thread_c;
-	arg_ptr = &arg_t;
 
 	/* Reading A and B from files */
+
 	fscanf(fptr1, "%d %d", &r_A, &c_A);
-	fscanf(fptr2, "%d %d", &r_B, &c_B);
 
 	while (!feof(fptr1)) {
 		fscanf(fptr1, "%s", readString);
@@ -63,23 +50,51 @@ int main(int argc, char ** argv)
 		}	
 	}
 
+	fscanf(fptr2, "%d %d", &r_B, &c_B);
+
 	while (!feof(fptr2)) {  
-                fscanf(fptr2, "%s", readString);
-                if (readString[0] == '#') {
-                        fscanf(fptr2, "%s", readString);
-                        fscanf(fptr2, "%s", readString);
-                        i = atoi(readString);
-                        j = 0;
-                } else {
-                        B[i][j] = atof(readString);
-                        j++;
-                }
-        }
-	
-	/* Threaded Matrix Multiplication */
-	for (i = 0; i < thread_c; i++) {
-		arg_t.t_n = i;
-		t_status = pthread_create(&threads[i], NULL, threaded_multiply, (void *) arg_ptr);
+		fscanf(fptr2, "%s", readString);
+		if (readString[0] == '#') {
+		    fscanf(fptr2, "%s", readString);
+		    fscanf(fptr2, "%s", readString);
+		    i = atoi(readString);
+		    j = 0;
+		} else {
+		    B[i][j] = atof(readString);
+		    j++;
+		}
+	}
+
+	if (c_A != r_B) {
+		printf("Incompatible matrices for matrix multiplication!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Work division among the required threads */
+
+	temp = r_A / t_c;
+	temp2 = r_A % t_c;
+
+	for (i = 0; i < t_c; i++) {
+		t_jobs[i] = temp;
+		if (temp2 > 0) {
+			t_jobs[i]++;
+			temp2--;
+		}
+	}
+
+	/* Verifying work division */
+
+	for (i = 0; i < t_c; i++) {
+		printf("%d\n", t_jobs[i]);
+	}
+
+	/* Threaded matrix multiplication */
+
+	for (i = 0; i < t_c; i++) {
+		t_n = i;
+
+		t_status = pthread_create(&threads[i], NULL, threaded_multiply, (void *)t_n);
 		
 		if (t_status) {
 			printf("Error - pthread_create() return code: %d\n", t_status);
@@ -87,12 +102,14 @@ int main(int argc, char ** argv)
 		}
 	}
 
-	for (i = 0; i < thread_c; i++) {
+	/* Joining the threads */
+	
+	for (i = 0; i < t_c; i++) {
 		pthread_join(threads[i], NULL);
 	}
-
+	
 	/* Verifying A and B */
-	/* 
+
 	for (i = 0; i < r_A; i++) {
 		for (j = 0; j < c_A; j++) {
 			printf("%lf ", A[i][j]);
@@ -101,14 +118,15 @@ int main(int argc, char ** argv)
 	}
 
 	for (i = 0; i < r_A; i++) {
-                for (j = 0; j < c_A; j++) {
-                        printf("%lf ", B[i][j]);
-                }
-                printf("\n");
+        for (j = 0; j < c_A; j++) {
+            printf("%lf ", B[i][j]);
         }
-	*/
+        printf("\n");
+    }
+	
 
 	/* Printing product matrix */
+
 	printf("%d %d\n", r_A, c_B);
 	for (i = 0; i < r_A; i++) {
 		printf("# Row %d\n", i);
@@ -120,20 +138,35 @@ int main(int argc, char ** argv)
 	return 0;
 }
 
-void * threaded_multiply(void * arg_x)
+void * threaded_multiply(void * arg_n)
 {
-	struct arg * arg_ptr = (struct arg *) arg_x;
+	int t_n = (int) arg_n;
 	int i;
 	int j;
 	int k;
+	int temp;
+	int temp2;
+	double temp_sum;
 
-	for (i = (arg_ptr->t_c * arg_ptr->t_n); i < ((arg_ptr->t_c * arg_ptr->t_n) + arg_ptr->t_c); i++) {
-		for (j = 0; j < arg_ptr->c_B; j++) {
+	temp2 = 0;
+
+	/* Determining start index of the current thread */
+
+	if (t_n != 0) {
+		for (temp = t_n - 1; temp >= 0; temp--) {
+			temp2 += t_jobs[temp];
+		}
+	}
+
+	/* Matrix multiplication */
+
+	for (i = temp2; i < (temp2 + t_jobs[t_n]); i++) {
+		for (j = 0; j < c_B; j++) {
 			C[i][j] = 0;
-			for (k = 0; k < arg_ptr->r_B; k++) {
+			for (k = 0; k < c_A; k++) {
 				C[i][j] += (A[i][k] * B[k][j]);
 			}
 		}
 	}
-		
 }
+
